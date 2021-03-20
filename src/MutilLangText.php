b12bfb03fb3d4aefcc690303e9bd3c8d3e27302f
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2021-03-20 09:47:52 +0100

    Improved MultiLangText util

--- src/MutilLangText.php
+++ src/MutilLangText.php
@@ -11,12 +11,25 @@ class MultiLangText
      */
     public function generate($text)
     {
-        $languages = Language::getLanguages();
-        $multilang = [];
-        foreach ($languages as $lang) {
-            $multilang[$lang['id_lang']] = $text;
+        $translatedNames = [];
+        foreach (Language::getLanguages() as $lang) {
+            // In case we just receive a string, we apply it to all languages
+            if (!is_array($text)) {
+                $translatedNames[$lang['id_lang']] = $text;
+            } elseif (array_key_exists($lang['locale'], $text)) {
+                $translatedNames[$lang['id_lang']] = $text[$lang['locale']];
+            } elseif (array_key_exists($lang['language_code'], $text)) {
+                $translatedNames[$lang['id_lang']] = $text[$lang['language_code']];
+            } elseif (array_key_exists($lang['iso_code'], $text)) {
+                $translatedNames[$lang['id_lang']] = $text[$lang['iso_code']];
+            } elseif (array_key_exists($lang['id_lang'], $text)) {
+                $translatedNames[$lang['id_lang']] = $text[$lang['id_lang']];
+            } else {
+                $translatedNames[$lang['id_lang']] = reset($text); // Get the first name available in the array
+            }
         }
-        return $multilang;
+
+        return $translatedNames;
     }
 
 }
